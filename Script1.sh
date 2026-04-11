fd53ad64176e96f61c2740a9ba2b2036697f0e1f
anupama994 <124217038+anupama994@users.noreply.github.com>
2026-04-11 23:11:04 +0530

    ram b1

--- Script1.sh
+++ Script1.sh
@@ -1,9 +1,4 @@
 echo "We are trying this practical from linux EC2"
 echo "I am from Bangalore"
 echo "Good Morning"
-echo "Hello All"
-pwd
-clear
-echo "Learning git"
-echo "new line in branch b1"
-echo "new2"
+echo "ram"
